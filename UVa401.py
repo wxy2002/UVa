e7bcc1e6b1f88981b6b2ea7690c5a98d0eea0076
Wu Xinyuan <837038599@qq.com>
2022-08-08 14:45:55 +0800

    accept UVa401

--- UVa401.py
+++ UVa401.py
@@ -1,20 +1,35 @@
-wrongstr = "`1234567890-=QWERTYUIOP[]\ASDFGHJKL;'ZXCVBNM,./"
-
 lines=[]
 while True:
     try:
         lines.append(input())
     except:
-        break 
+        break
 
-def do_line(in_str):
-    for i in range(len(in_str)):
-        x = wrongstr.find(in_str[i])
-        if x == -1:
-            print (in_str[i], end='')
-        else:
-            print (wrongstr[x-1], end='')
-    print ('')
+def pal(s):
+    for i in range(len(s)):
+        if s[i]!=s[len(s) - 1 - i]:
+            return 0
+    return 1
+
+mir_dic = {'A':'A', 'E':'3', 'H':'H', 'I':'I', 'J':'L', 'L':'J', 'M':'M', 'O':'O', 'S':'2', 'T':'T', 'U':'U', 'V':'V', 'W':'W', 'X':'X', 'Y':'Y', 'Z':'5', '1':'1', '2':'S',
+'3':'E', '5':'Z', '8':'8'
+}
+
+def mir(s):
+    for i in range(len(s)):
+        if (s[i] not in mir_dic.keys()) or ((s[i] in mir_dic.keys()) and (mir_dic[s[i]]!=s[len(s) - 1 - i])):
+            return 0
+    return 1
 
 for i in range(len(lines)):
-    do_line(lines[i])
+    s = lines[i]
+    flag_pal = pal(s)
+    flag_mir = mir(s)
+    if flag_pal==0 and flag_mir==0:
+        print (s, '-- is not a palindrome.\n')
+    if flag_pal==1 and flag_mir==0:
+        print (s, '-- is a regular palindrome.\n')
+    if flag_pal==0 and flag_mir==1:
+        print (s, '-- is a mirrored string.\n')
+    if flag_pal==1 and flag_mir==1:
+        print (s, '-- is a mirrored palindrome.\n')
